5faba0608bd9b52f40e928f93372610567d16f7b
Scott Doxey <neogeek0101@gmail.com>
2015-05-31 13:57:16 -0400

    Added update file.

---
+++ bin/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR=$(cd "$(dirname "${BASH_SOURCE}")" && cd ../ && pwd)
+
+echo -n "Updating dotfiles to latest ... "
+
+(cd ${DIR} && git pull &> /dev/null)
+
+echo "Done"
+
+(cd ${DIR} && source bootstrap.sh)
