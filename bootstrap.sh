72dfd1f336bcdcf34d624abebda80d41e718d516
Scott Doxey <neogeek0101@gmail.com>
2015-05-31 13:50:39 -0400

    Added description of what is happening in bootstrap file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,14 @@
 
 DIR=$(cd "$(dirname "${BASH_SOURCE}")" && pwd)
 
+echo -n "Installing @neogeek's dotfiles to ~/.bash_profile ... "
+
 touch ~/.bash_profile
 
 sed -ie "/#dotfiles/d" ~/.bash_profile
 
 echo "source ${DIR}/.bash_profile #dotfiles" >> ~/.bash_profile
+
+echo "Done"
 
 source ~/.bash_profile
