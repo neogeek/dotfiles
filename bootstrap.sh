63e7047ef8531a8d9a3afd6b5176ddc85a440f0d
Scott Doxey <neogeek0101@gmail.com>
2015-10-15 17:33:02 -0400

    Reordered bootstrap file.
    Added git autocomplete install notice.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,25 @@
 
 DIR=$(cd "$(dirname "${BASH_SOURCE}")" && pwd)
 
-echo -n "Installing @neogeek's dotfiles to ~/.bash_profile ... "
+# Install Git Autocomplete
+
+echo -n "Installing git autocomplete ... "
+
+mkdir -p "${DIR}/vendor/"
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "${DIR}/vendor/git-completion.bash" &> /dev/null
+
+echo "Done"
 
 # Update .bash_profile
+
+echo -n "Installing @neogeek's dotfiles to ~/.bash_profile ... "
 
 touch ~/.bash_profile
 
 sed -ie "/#dotfiles/d" ~/.bash_profile
 
 echo "source ${DIR}/.bash_profile #dotfiles" >> ~/.bash_profile
-
-# Install Git Autocomplete
-
-mkdir -p "${DIR}/vendor/"
-
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "${DIR}/vendor/git-completion.bash" &> /dev/null
 
 echo "Done"
 
