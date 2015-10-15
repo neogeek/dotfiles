f413d97eb8d11bd4f3e786ffd771daba04b26956
Scott Doxey <neogeek0101@gmail.com>
2015-10-15 17:30:41 -0400

    Added git autocomplete.

--- .bash_profile
+++ .bash_profile
@@ -5,7 +5,7 @@ DIR=$(dirname "${BASH_SOURCE}")
 export PATH="${DIR}/bin:${PATH}";
 
 for file in "${DIR}"/.{aliases,custom,functions,gitsetup,theme}; do
-    if [ -a "$file" ]; then
+    if [ -f "$file" ]; then
         # shellcheck source=/dev/null
         source "${file}"
     fi
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,19 @@ DIR=$(cd "$(dirname "${BASH_SOURCE}")" && pwd)
 
 echo -n "Installing @neogeek's dotfiles to ~/.bash_profile ... "
 
+# Update .bash_profile
+
 touch ~/.bash_profile
 
 sed -ie "/#dotfiles/d" ~/.bash_profile
 
 echo "source ${DIR}/.bash_profile #dotfiles" >> ~/.bash_profile
+
+# Install Git Autocomplete
+
+mkdir -p "${DIR}/vendor/"
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "${DIR}/vendor/git-completion.bash" &> /dev/null
 
 echo "Done"
 
