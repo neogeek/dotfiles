95a1f0467fb4f17cfd04fb22e460afbe0d4b513d
Scott Doxey <neogeek0101@gmail.com>
2016-03-18 19:04:48 -0400

    Added install script.

---
+++ bin/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+source brew.sh
+
+# shellcheck disable=SC1091
+source gem.sh
+
+# shellcheck disable=SC1091
+source npm.sh
+
+# shellcheck disable=SC1091
+source osx.sh
