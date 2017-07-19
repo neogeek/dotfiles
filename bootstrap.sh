50d1e3426d27cfe392d64b9dcfe81c1fa77a4840
Scott Doxey <neogeek0101@gmail.com>
2017-07-18 20:37:01 -0400

    Locked version numbers for each git plugin.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,9 @@ echo -n "Installing (3) git plugins ... "
 
 mkdir -p "${DIR}/vendor/"
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "${DIR}/vendor/git-completion.bash" &> /dev/null
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o "${DIR}/vendor/git-prompt.bash" &> /dev/null
-curl https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight -o "${DIR}/vendor/diff-highlight" &> /dev/null
+curl https://raw.githubusercontent.com/git/git/cac25fc330fc26050dcbc92c4bfff169a4848e93/contrib/completion/git-completion.bash -o "${DIR}/vendor/git-completion.bash" &> /dev/null
+curl https://raw.githubusercontent.com/git/git/cac25fc330fc26050dcbc92c4bfff169a4848e93/contrib/completion/git-prompt.sh -o "${DIR}/vendor/git-prompt.bash" &> /dev/null
+curl https://raw.githubusercontent.com/git/git/2bc81f2a83601ddfaad7d38406f819682f9cb756/contrib/diff-highlight/diff-highlight -o "${DIR}/vendor/diff-highlight" &> /dev/null
 chmod +x "${DIR}/vendor/diff-highlight"
 
 echo "Done"
