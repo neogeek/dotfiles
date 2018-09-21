8f6510a4c9a00c145170195f60398f09e39dd21b
Scott Doxey <neogeek0101@gmail.com>
2018-09-20 21:15:05 -0400

    Removed Unity PackageManager chown command.

--- bin/unity.sh
+++ bin/unity.sh
@@ -9,5 +9,3 @@ if [ -d $UNITY_TEMPLATE_DIR ]; then
     cp -R "${CWD}/../unity/scripttemplates/" $UNITY_TEMPLATE_DIR
 
 fi
-
-(cd /Applications/Unity/Unity.app/Contents/Resources/PackageManager && sudo chown -R "$(whoami)" ./)
