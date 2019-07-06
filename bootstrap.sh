de827064ee9d412c1de587fdcc5324c369d93960
Scott Doxey <neogeek0101@gmail.com>
2019-07-06 18:02:12 -0400

    Switched from diff-highlight to diff-so-fancy.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,9 @@ mkdir -p "${DIR}/vendor/"
 
 curl https://raw.githubusercontent.com/git/git/cac25fc330fc26050dcbc92c4bfff169a4848e93/contrib/completion/git-completion.bash -o "${DIR}/vendor/git-completion.bash" &> /dev/null
 curl https://raw.githubusercontent.com/git/git/cac25fc330fc26050dcbc92c4bfff169a4848e93/contrib/completion/git-prompt.sh -o "${DIR}/vendor/git-prompt.bash" &> /dev/null
-curl https://raw.githubusercontent.com/git/git/2bc81f2a83601ddfaad7d38406f819682f9cb756/contrib/diff-highlight/diff-highlight -o "${DIR}/vendor/diff-highlight" &> /dev/null
-chmod +x "${DIR}/vendor/diff-highlight"
+
+curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -o "${DIR}/vendor/diff-so-fancy" &> /dev/null
+chmod +x "${DIR}/vendor/diff-so-fancy"
 
 echo "Done"
 
