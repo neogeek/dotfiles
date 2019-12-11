41b2380d42eca6a7c917c47ecdec1a4d1dd55315
neogeek <neogeek0101@gmail.com>
2019-12-10 22:37:42 -0500

    Updated ruby version.

--- bin/gem.sh
+++ bin/gem.sh
@@ -20,10 +20,10 @@ fi
 # shellcheck disable=SC1090
 source ~/.bash_profile
 
-if ! grep "2.6.2" <<< "$(rbenv version)"; then
+if ! grep "2.6.5" <<< "$(rbenv version)"; then
 
-    rbenv install 2.6.2
-    rbenv global 2.6.2
+    rbenv install 2.6.5
+    rbenv global 2.6.5
 
 fi
 
