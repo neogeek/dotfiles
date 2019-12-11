39f2694a59bb21379be331f1e1d2ae06e58f1684
neogeek <neogeek0101@gmail.com>
2019-12-10 22:41:19 -0500

    Updated nvm version.

--- bin/npm.sh
+++ bin/npm.sh
@@ -16,7 +16,7 @@ if [ ! -a ~/.nvm/nvm.sh ]; then
 
     echo "Installing NVM"
 
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.1/install.sh | bash
 
 fi
 
