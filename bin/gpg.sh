1d42ca85d0f2f87554adcb65514f0d0087c0d609
Scott Doxey <neogeek0101@gmail.com>
2021-11-17 23:31:09 -0500

    Updated gpg to work with latest packages.

--- bin/gpg.sh
+++ bin/gpg.sh
@@ -13,7 +13,7 @@ brew install gnupg pinentry-mac
 if hash gpg &>/dev/null; then
 
     git config --global commit.gpgsign true
-    git config --global gpg.program /usr/local/bin/gpg
+    git config --global gpg.program $(which gpg)
 
 fi
 
@@ -22,11 +22,11 @@ if [ -d ~/.gnupg/ ]; then
     sed -ie "/no-tty/d" ~/.gnupg/gpg.conf
     sed -ie "/use-agent/d" ~/.gnupg/gpg.conf
 
-    echo "no-tty" >>~/.gnupg/gpg.conf
-    echo "use-agent" >>~/.gnupg/gpg.conf
+    echo "no-tty" >> ~/.gnupg/gpg.conf
+    echo "use-agent" >> ~/.gnupg/gpg.conf
 
-    sed -ie "/pinentry-program \/usr\/local\/bin\/pinentry-mac/d" ~/.gnupg/gpg-agent.conf
+    sed -ie "/pinentry-program \/usr\/homebrew\/bin\/pinentry-mac/d" ~/.gnupg/gpg-agent.conf
 
-    echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
+    echo "pinentry-program $(which pinentry-mac)" >> ~/.gnupg/gpg-agent.conf
 
 fi
