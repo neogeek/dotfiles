12819bc27d926972faba52bfc6df156a4f46cb01
Scott Doxey <neogeek0101@gmail.com>
2025-09-21 15:00:45 -0400

    Update npm.sh

--- bin/npm.sh
+++ bin/npm.sh
@@ -15,7 +15,7 @@ done
 if [ ! -a ~/.nvm/nvm.sh ]; then
     echo "Installing NVM"
 
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 fi
 
 # shellcheck disable=SC1090
@@ -23,23 +23,22 @@ source ~/.nvm/nvm.sh
 
 PREVIOUS_DEFAULT_VERSION=$(node --version)
 
-nvm install 20 --latest-npm
 nvm install 22 --latest-npm
+nvm install 24 --latest-npm
 
-nvm alias default 20
+nvm alias default 22
 
 npm config set save=true
 npm config set save-exact=true
 npm config set progress=true
+npm config set ignore-scripts=true
 
-if [[ "${PREVIOUS_DEFAULT_VERSION}" != $(node --version) ]]; then
-    npm install -g get-unity
-    npm install -g nodemon
-    npm install -g npm-check-updates
-    npm install -g pm2
-    npm install -g svgo
-    npm install -g unity-check-updates
-fi
+npm install -g get-unity
+npm install -g nodemon
+npm install -g npm-check-updates
+npm install -g pm2
+npm install -g svgo
+npm install -g unity-check-updates
 
 if [[ $(cat ~/.zshrc) != *"begin-npm-completion"* ]]; then
     npm completion >>~/.zshrc
